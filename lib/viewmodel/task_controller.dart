ba3c3adf17b843c7364099048140b0f13bfa0051
Rinku <merinkukumar69@gmail.com>
2024-10-17 22:55:49 +0530

    method of task perform using controller

---
+++ lib/viewmodel/task_controller.dart
@@ -0,0 +1,42 @@
+
+
+import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:todo_app/data/task.dart';
+
+class TaskController extends GetxController {
+
+  var tasks = <Task>[].obs;
+  late Box<Task> taskBox;
+
+  @override
+  void onInit() {
+    super.onInit();
+    taskBox = Hive.box<Task>('tasks');
+    tasks.value = taskBox.values.toList();
+  }
+
+  void addTask(Task task){
+    taskBox.add(task);
+    tasks.add(task);
+  }
+
+  void editTask(int index,Task task){
+    taskBox.putAt(index, task);
+    tasks[index] = task;
+  }
+
+  void deleteTask(int index){
+    taskBox.deleteAt(index);
+    tasks.remove(index);
+  }
+
+  void sortTaskByPriority(){
+    tasks.sort((a,b) => a.priority.compareTo(b.priority));
+  }
+
+  void sortTaskByDueDate(){
+    tasks.sort((a,b) => a.dueDate.compareTo(b.dueDate));
+  }
+
+}
